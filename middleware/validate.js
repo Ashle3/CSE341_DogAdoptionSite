56fe7cd04187d4798b1c0db460c2eed9f17a7b5d
Ashle3 <ashleeb3@byui.edu>
2023-10-28 21:28:15 -0500

    added saveLargeDog function to validate.js

--- middleware/validate.js
+++ middleware/validate.js
@@ -23,6 +23,30 @@ const saveSmallDog = (req, res, next) => {
   });
 };
 
+const saveLargeDog = (req, res, next) => {
+  const validationRule = {
+    name: 'required|string',
+    breed: 'required|string',
+    classification: 'required|string',
+    gender: 'required|string',
+    age: 'required|string',
+    furColor: 'required|string',
+    weight: 'required|integer'
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
+
 module.exports = {
-  saveSmallDog
+  saveSmallDog,
+  saveLargeDog
 };
